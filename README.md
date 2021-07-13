4ec2d5fe027ea21d5e80a1f3b3f4bf46206d2353
James Birkett <git@birkett83.net>
2021-07-13 21:29:07 +1000

    Landgrab Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # landgrab
-A mini-game based on ingress and IITC
+A mini-game based on ingress and IITC.
+
+## What is ingress?
+[Ingress](https://ingress.com/) is an augmented-reality game for Android and
+iOS.
+
+## What is IITC?
+[Ingress Intel Total Conversion](https://iitc.app/) is a fan-made replacement
+for the standard Ingress online map.
+
+# What is Landgrab?
+Landgrab is a mini-game built using IITC.
+
+Ingress tracks which portals you have captured. Landgrab gives you a score for
+each portal based on how many nearby portals you have captured. You get a lot
+more points for capturing all of the portals in an area than you would for
+capturing the same number of portals spread out more widely.
+
+## How does it work?
+For each portal you have captured, Landgrab draws a circle centred on that
+portal and passing through the nearest portal that you *haven't* captured. Your
+score for that portal is the number of portals inside the circle. Your total
+landgrab score is the sum of all of the scores of each portal.
+
+## How do I play it?
+You need a userscript manager to use IITC, install the landgrab plugin in the
+same way.
+
+## This is flakey as heck, what's the deal?
+- I don't know javascript. I r clueless noob.
+- There is a bug related to caching in the standard Ingress Intel map that
+  causes your portal capture history to not load correctly. If you reload the
+  page, the history for the portals in view on the map should load correctly.
+  Landgrab will store history in your browser once it has seen it, so you should
+  only need to do this once for each area.
+- Scores are currently only computed whenever new portal data loads. This
+  happens either if you refresh the page or if you scroll to a new area. If you
+  have just captured a portal and want the score to update immediately, try
+  reloading the page. I r javascript noob, soz.
+- It's slow. I don't know how much of this is Intel, how much is IITC and how
+  much is Landgrab. Um. Did I mention I'm a javascript noob? I did use a
+  quadtree for O(n log(n)) performance instead of the naive O(n^2) algorithm. Do  I get points for that? I'm sorry.
+
+# Credits
+IITC was originally written by Stefan Breunig and IITC "Community Edition"
+(IITC-CE) is now maintained by modos189 and others.
+
+Landgrab was originally based on the Uniques plugin by 3ch01c.
---
+++ landgrab.js
@@ -0,0 +1,426 @@
+// ==UserScript==
+// @author         birkett83
+// @name           IITC plugin: landgrab
+// @category       Misc
+// @version        0.1
+// @description    landgrab
+// @id             landgrab
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
+
+function wrapper(plugin_info) {
+
+    // Ingress gives coords stored as (lat, lng) in micro-degrees.
+    // Most significant bit of 360,000,000 is 1<<28
+    // So we need 28 bits and our max tree height is 28.
+    const MAX_HEIGHT = 28;
+    // Using negative numbers in the quad tree was giving me a headache.
+    // So we'll add 180° before storing / looking up in quad tree.
+    const OFFSET = 180*1000*1000;
+
+    // ensure plugin framework is there, even if iitc is not yet loaded
+    if(typeof window.plugin !== 'function') window.plugin = function() {};
+
+    //use own namespace for plugin
+    var landgrab = window.plugin.landgrab = function() {};
+
+    // maps the JS property names to localStorage keys
+    landgrab.FIELDS = {
+        'portalInfo': 'plugin-landgrab-portalinfo',
+        'quadtree': 'plugins-landgrab-quadtree',
+    };
+
+    landgrab.portalInfo = {};
+    landgrab.quadtree = [];
+    landgrab.updateQueue = [];
+    landgrab.score = 0;
+
+    landgrab.disabledMessage = null;
+    landgrab.contentHTML = null;
+
+    landgrab.isHighlightActive = false;
+
+    landgrab.onPortalDetailsUpdated = function() {
+        if(typeof(Storage) === "undefined") {
+            $('#portaldetails > .imgpreview').after(landgrab.disabledMessage);
+            return;
+        }
+
+        var guid = window.selectedPortal,
+            details = portalDetail.get(guid),
+            nickname = window.PLAYER.nickname;
+        if(details) {
+            if(details.history) {
+                if(details.history.captured) {
+                    landgrab.setPortalCaptured(guid);
+                }
+            }
+            details.score = landgrab.portalScore(guid);
+            $('#portaldetails > .imgpreview').after(
+                '<div id="landgrab-container">Portal Score: ' + details.score + '</br>' +
+                'Total Score: ' + landgrab.score + '</div>');
+        }
+    }
+
+    landgrab.onPortalAdded = function (data) {
+        let guid = data.portal.options.guid;
+        //landgrab.updateQueue.push(guid);
+        let portal = data.portal;
+        let history = portal.options.data.history;
+        // Bug in stock ingress means history often doesn't show. Something about caching.
+        // Reload the page and eventually it does. Or something.
+        // For now we just won't add portals until we get the history.
+        if (history) {
+            let portalInfo = landgrab.portalInfo[guid];
+            if (!portalInfo) {
+                //console.log("Adding", guid);
+                portalInfo = landgrab.portalInfo[guid] = {
+                    captured: history.captured,
+                    score: 0,
+                    lat: portal.options.data.latE6,
+                    lng: portal.options.data.lngE6,
+                };
+            } else {
+                //console.log("Updating history on", guid);
+                portalInfo.captured = history.captured;
+            }
+            // add the portal to the queue for rescoring.
+            // no point rescoring now because more portals will likely be added nearby
+            landgrab.updateQueue.push(guid);
+            landgrab.addPortalToQuadTree(portal);
+
+        } else {
+            //console.log("History not found; Not adding", guid)
+        }
+    }
+
+    landgrab.onMapDataRefreshEnd = function () {
+        var guid;
+        while (guid = landgrab.updateQueue.pop()) {
+            landgrab.portalScore(guid);
+        }
+        landgrab.score = 0;
+        for (let guid in landgrab.portalInfo) {
+            landgrab.score += landgrab.portalInfo[guid].score;
+        }
+        landgrab.storeLocal('portalInfo');
+        landgrab.storeLocal('quadtree');
+    }
+
+    landgrab.setPortalCaptured = function(guid) {
+        var portalInfo = landgrab.portalInfo[guid];
+
+        if(portalInfo === undefined) {
+            return;
+        }
+
+        if(portalInfo.captured) return;
+
+        portalInfo.captured = true;
+        landgrab.storeLocal('portalInfo');
+    }
+
+    landgrab.addPortalToQuadTree = function(portal) {
+        var lat = portal.options.data.latE6 + OFFSET;
+        var lng = portal.options.data.lngE6 + OFFSET;
+
+        var tree = landgrab.quadtree;
+
+        for (let height = MAX_HEIGHT; height > 0; height--) {
+            if (tree.length == 0) {
+                // create a new empty quadtree node
+                for (let i = 0; i < 4; i++) {
+                    tree[i] = [];
+                }
+            }
+            let quadrant = (((lat >> height) & 1) << 1)| ((lng >> height) & 1);
+            tree = tree[quadrant];
+        }
+        let quadrant = ((lat & 1) << 1 ) | (lng & 1);
+        tree[quadrant] = portal.options.guid;
+    }
+
+    landgrab.distPointPoint = function(lat, lng, other_lat, other_lng) {
+        if (other_lat > OFFSET || other_lng > OFFSET) {
+            console.warn(other_lng, " > ", OFFSET, "Did you get this from a quad?");
+        }
+        // spherical distance formula
+        // stolen from https://www.movable-type.co.uk/scripts/latlong.html
+        // conversion from micro-degrees to radians
+        const factor = Math.PI/180000000
+        let φ1 = lat * factor;
+        let φ2 = lat * factor;
+        let Δφ = (lat-other_lat) * factor;
+        let Δλ = (lng-other_lng) * factor;
+
+        let a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
+        return 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    }
+
+    landgrab.distPointQuad = function(lat, lng, quad_lat, quad_lng, height) {
+        // The quad is the square with top left corner (quad_lat, quad_lng)
+        // and side length 1<<height
+
+        // The final `height` bits of quad_lat, quad_lng should be 0.
+        // The bitmask has those bit set to 1 so the far corner of the quad is
+        // (quad_lat | bitmask, quad_lng | bitmask)
+
+        let bitmask = (1 << height) - 1;
+
+        let quad_lat_far = quad_lat | bitmask;
+        let quad_lng_far = quad_lng | bitmask;
+
+        const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
+        var nearest_lat = clamp(lat+OFFSET, quad_lat, quad_lat_far);
+        var nearest_lng = clamp(lng+OFFSET, quad_lng, quad_lng_far);
+
+        // undo the offset used for storing points in the tree
+        var dst = landgrab.distPointPoint(lat, lng, nearest_lat - OFFSET, nearest_lng - OFFSET);
+
+        return dst;
+    }
+
+
+    landgrab.findNearest = function(lat, lng, pred, max_dst, trace) {
+
+        let tree = landgrab.quadtree;
+        if (max_dst === undefined) max_dst = Infinity;
+
+        return landgrab.findNearestInner(tree, lat, lng, 0, 0, MAX_HEIGHT+1, null, max_dst, pred, trace);
+    }
+
+    landgrab.findNearestInner = function(tree, lat, lng, quad_lat, quad_lng, height, cur_best, cur_dst, pred, trace) {
+        ////let prefix = -(1 << height);
+        let bitmask = (1 << height) - 1;
+        let prefix = ~bitmask;
+        const bin = function(num) {
+            return (num & ~(1 <<31)).toString(2).padStart(31, 0)
+        }
+        // useful for debugging
+        const target_in_quad = (lat, lng) => (((((lat + OFFSET) & prefix) ^ quad_lat) == 0) && ((((lng + OFFSET & prefix) ^ quad_lng) == 0)));
+        if ( trace ) {
+            // useful for debugging
+            console.log("Target coords:     ", bin(lat + OFFSET), bin(lng + OFFSET));
+            console.log("    Looking in quad", bin(quad_lat), bin(quad_lng), height);
+            console.log("             Prefix", bin(prefix), bin(prefix))
+        }
+        if(pred === undefined) { console.log("pred is undefined"); pred = () => true;}
+        if (!tree || tree.length == 0) {
+            if ( trace ) {
+                console.log("bailing because tree empty", tree);
+            }
+            return [cur_best, cur_dst];
+        }
+        if (height == 0) {
+            // console.log(tree);
+            // tree is a leaf node, i.e a portal guid
+            let portalInfo = landgrab.portalInfo[tree];
+            let new_dst = landgrab.distPointPoint(lat, lng, portalInfo.lat, portalInfo.lng);
+            if (new_dst < cur_dst) {
+                if (trace) console.log("calling pred");
+                if (pred(tree)) {
+                    //console.log(tree);
+                    return [tree, new_dst];
+                } else {
+                    return [cur_best, cur_dst];
+                }
+            } else {
+                return [cur_best, cur_dst];
+            }
+        }
+
+        let new_dst = landgrab.distPointQuad(lat, lng, quad_lat, quad_lng, height)
+
+        if (new_dst > cur_dst) {
+            if ( trace ) {
+                console.log("bailing because of dst", cur_dst, new_dst);
+                console.log(lat, lng, quad_lat, quad_lng, height);
+            }
+
+            return [cur_best, cur_dst];
+        }
+
+        height = height - 1;
+
+        bitmask = 1 << height;
+
+        [cur_best, cur_dst] = landgrab.findNearestInner(tree[0], lat, lng, quad_lat, quad_lng, height, cur_best, cur_dst, pred, trace);
+        [cur_best, cur_dst] = landgrab.findNearestInner(tree[1], lat, lng, quad_lat, quad_lng | bitmask, height, cur_best, cur_dst, pred, trace);
+        [cur_best, cur_dst] = landgrab.findNearestInner(tree[2], lat, lng, quad_lat | bitmask, quad_lng, height, cur_best, cur_dst, pred, trace);
+        [cur_best, cur_dst] = landgrab.findNearestInner(tree[3], lat, lng, quad_lat | bitmask, quad_lng | bitmask, height, cur_best, cur_dst, pred, trace);
+        return [cur_best, cur_dst];
+    }
+
+    landgrab.portalScore = function(guid) {
+        var portalInfo = landgrab.portalInfo[guid];
+        if (!portalInfo) {
+            console.log("guid not found", guid);
+            return 0;
+        }
+        if (!portalInfo.captured) return 0;
+        var [nearest_uncaptured, dist] = landgrab.findNearestUncaptured(portalInfo.lat, portalInfo.lng);
+        let score = function (guid) {
+            score.count = score.count + 1;
+            return false;
+        }
+        score.count = 0;
+        landgrab.findNearest(portalInfo.lat, portalInfo.lng, score, dist);
+        portalInfo.score = score.count;
+        return score.count;
+    }
+
+    landgrab.findNearestUncaptured = function(lat, lng) {
+        var uncaptured = function(guid) {
+            return !landgrab.portalInfo[guid].captured;
+        }
+        return landgrab.findNearest(lat, lng, uncaptured);
+    }
+
+    /*landgrab.findExact = function (lat, lng) {
+        var tree = landgrab.quadtree;
+        for (let height = MAX_HEIGHT; height > 0; height--) {
+            let quadrant = (((lat >> height) & 1) << 1)| ((lng >> height) & 1);
+
+            if (tree[quadrant].length == 0) {
+                console.log(height, quadrant, tree);
+            }
+            tree = tree[quadrant];
+        }
+        let quadrant = ((lat & 1) << 1 ) | (lng & 1);
+        return tree[quadrant];
+    }*/
+
+    /*landgrab.commonPrefixDist = function(lat, lng) {
+        // Finds the quadrant in the tree with the longest common prefix of lat, lng.
+        // This gives us an upper bound for the distance to the nearest point.
+        var tree = landgrab.quadtree;
+        for (let height = 31; height >= 0; height--) {
+            if (tree.length > 0) {
+                let quadrant = (((lat >> (height-1)) & 2) | ((lng >> height) & 1));
+                //console.log(tree, height);
+                tree = tree[quadrant];
+            } else {
+                // This quad is not in the quadtree. so parent is, which means the target
+                // point is in a quad of height height+1 that contains at least one portal
+                // The target point is at most sqrt(2)*(1<<(height+1)) from that portal.
+                // But... we're using squared distances because taking square roots is for
+                // suckers.
+
+                return Math.pow(2, 2*height+3);
+            }
+        }
+        return 0;
+    }*/
+
+    landgrab.storeLocal = function(name) {
+        var key = landgrab.FIELDS[name];
+        if(key === undefined) return;
+
+        var value = landgrab[name];
+
+        if(typeof value !== 'undefined' && value !== null) {
+            localStorage[key] = JSON.stringify(landgrab[name]);
+        } else {
+            localStorage.removeItem(key);
+        }
+    }
+
+    landgrab.loadLocal = function(name) {
+        var key = landgrab.FIELDS[name];
+        if(key === undefined) return;
+
+        if(localStorage[key] !== undefined) {
+            landgrab[name] = JSON.parse(localStorage[key]);
+        }
+    }
+
+    /***************************************************************************************************************************************************************/
+    /** HIGHLIGHTER ************************************************************************************************************************************************/
+    /***************************************************************************************************************************************************************/
+    landgrab.highlighter = {
+        highlight: function(data) {
+            var guid = data.portal.options.ent[0];
+            var history = data.portal.options.data.history;
+
+            var portalInfo = landgrab.portalInfo[guid];
+
+            // doing this here feels kinda gross. I can't find a better alternative right now.
+
+            if (history && portalInfo) {
+                if (history.captured) {
+                    landgrab.setPortalCaptured(guid);
+                }
+            }
+
+            var style = {};
+
+            if (portalInfo) {
+                if (portalInfo.captured) {
+                    // captured - no highlights
+                } else {
+                    // we have an 'portalInfo' entry for the portal, but it's not captured
+                    style.fillColor = 'white';
+                    style.fillOpacity = 1.0;
+                }
+            } else {
+                // no visit data at all
+                style.fillColor = 'white';
+                style.fillOpacity = 1.0;
+            }
+
+            data.portal.setStyle(style);
+        },
+
+        setSelected: function(active) {
+            landgrab.isHighlightActive = active;
+        }
+    }
+
+
+    landgrab.setupCSS = function() {
+        $("<style>")
+            .prop("type", "text/css")
+            .html('\
+#landgrab-container {\
+  display: block;\
+  text-align: center;\
+  margin: 6px 3px 1px 3px;\
+  padding: 0 4px;\
+}\
+#landgrab-container label {\
+  margin: 0 0.5em;\
+}\
+')
+            .appendTo("head");
+    }
+
+    landgrab.setupContent = function() {
+        landgrab.disabledMessage = '<div id="landgrab-container" class="help" title="Your browser does not support localStorage">Plugin landgrab disabled</div>';
+    }
+
+    var setup = function() {
+        landgrab.setupCSS();
+        landgrab.setupContent();
+        landgrab.loadLocal('portalInfo');
+        landgrab.loadLocal('quadtree');
+        window.addPortalHighlighter('landgrab', landgrab.highlighter);
+        window.addHook('portalDetailsUpdated', landgrab.onPortalDetailsUpdated);
+        window.addHook('portalAdded', landgrab.onPortalAdded);
+        window.addHook('mapDataRefreshEnd', landgrab.onMapDataRefreshEnd);
+    }
+
+    setup.info = plugin_info; //add the script info data to the function as a property
+    if(!window.bootPlugins) window.bootPlugins = [];
+    window.bootPlugins.push(setup);
+    // if IITC has already booted, immediately run the 'setup' function
+    if(window.iitcLoaded && typeof setup === 'function') setup();
+} // wrapper end
+// inject code into site context
+var script = document.createElement('script');
+var info = {};
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
+script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
+(document.body || document.head || document.documentElement).appendChild(script);
